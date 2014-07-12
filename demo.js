f83c40c02f0b8faebb7832e6bc7e15f608c35d52
SunPeter <ssd3237649@gmail.com>
2014-07-12 14:30:47 +0800

    pure socket.io

--- demo.js
+++ demo.js
@@ -1,17 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require("http");
+var path=require("path");
+var fs=require("fs");
 
-app.get('/', function(req, res){
-  res.sendfile('views/demo.html');
+var server=http.createServer(function(req,res){
+    fs.readFile(__dirname+"/views/demo.html",function(err,data){
+        if(err){
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        else{
+            res.writeHeader(200,{"Content-Type":"text/html"});
+            res.end(data);
+        }
+    })
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+var io=require("socket.io")(server);
+io.on("connection",function(socket){
+    socket.on("MSG",function(data){
+          //对当前客户端
+          socket.emit("MSG",data);
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+          //对除开当前的其他客户端
+          socket.broadcast.emit("MSG",data);
+
+          //所有客户端
+          io.sockets.emit('MSG', data);
+
+    })
 });
+server.listen(8080);
